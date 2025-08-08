cb46154f31ea3f10766125e57908ea11aebc8e78
mahesh <dev030.rejoice@gmail.com>
2025-08-08 16:31:06 +0530

    hero section updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import Footer from "@/components/footer";
-import client from "@/graphql/graphQLClient";
-import { ApolloProvider } from "@apollo/client";
 import Providers from "./providers"
 import ApolloClientProvider from "@/components/ApolloClientProvider";
 import { Toaster } from "react-hot-toast";
@@ -48,7 +46,6 @@ export default function RootLayout({ children }) {
         <link rel="preload" as="video" href="/assets/video/video.mp4" fetchPriority="high" />
       </head>
       <body className={`${poppins.variable} ${unbounded.variable} ${plus_jakarta.variable}`}>
-
         <Providers>
           <Toaster position="top-right" />
           <Header />
@@ -56,7 +53,6 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </Providers>
-
       </body >
     </html >
   );
